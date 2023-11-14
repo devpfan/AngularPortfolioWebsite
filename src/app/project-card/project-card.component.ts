e5833e6a491b1585f7b3d5fa388b1c0313f88736
devpfan <128647947+devpfan@users.noreply.github.com>
2023-11-14 11:56:46 -0500

    agregando modal con ngx-boostrap

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../_models/Project';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ProjectModalComponent } from '../project-modal/project-modal.component';
 
 @Component({
   selector: 'app-project-card',
@@ -9,5 +11,16 @@ import { Project } from '../_models/Project';
 export class ProjectCardComponent {
 
   @Input() project = {} as Project;
+  bsModelRef?: BsModalRef;
+
+  constructor(private modalService: BsModalService){
+
+
+    
+  }
+
+  OpenProjectModal(){
+    this.bsModelRef = this.modalService.show(ProjectModalComponent);
+  }
 
 }
